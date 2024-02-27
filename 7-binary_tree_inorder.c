f8436ef10cd37ca667368ce9ad7e52816b28fcdc
bikaze <clmntmugisha@gmail.com>
2024-02-27 18:52:27 +0200

    no no no

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -11,8 +11,6 @@
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree || !func)
-		return;
 	if (tree->left)
 		binary_tree_inorder(tree->left, func);
 	func(tree->n);
