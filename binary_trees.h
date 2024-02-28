9de4a34f5151fb1226c9fe07d89af379852d7661
ndiramiye2 <onlythenotes@gmail.com>
2024-02-28 22:36:33 +0200

    Nitafanya hivyo peke yangu

--- binary_trees.h
+++ binary_trees.h
@@ -1,11 +1,5 @@
 #ifndef BINARY_TREE_H
 #define BINARY_TREE_H
-<<<<<<< HEAD
-
-#include <stdio.h>
-#include <stdlib.h>
-=======
->>>>>>> 8a60e9d (for samoling)
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -15,24 +9,18 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-
-#include <stdlib.h>
-#include <stdio.h>
-
 struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-};
+} typedef struct binary_tree_s binary_tree_t;
+#include <stdlib.h>
+#include <stdio.h>
 
-typedef struct binary_tree_s binary_tree_t;
 void binary_tree_print(const binary_tree_t *);
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-<<<<<<< HEAD
-void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
@@ -48,13 +36,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 size_t binary_tree_depth(const binary_tree_t *tree);
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
-=======
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-int binary_tree_is_leaf(const binary_tree_t *node);
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
-size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
->>>>>>> 8a60e9d (for samoling)
+
 #endif
