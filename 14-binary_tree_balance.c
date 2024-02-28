c84783537ed0ce87453fd1a3ecbfc7ec10b4a978
ndiramiye2 <onlythenotes@gmail.com>
2024-02-28 23:53:11 +0200

    Nitafanya hivyo peke yangu

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,13 +8,12 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+		int left_height, right_height;
 
-    if (tree == NULL)
-        return 0;
+		if (tree == NULL)
+			return (0);
+		left_height = binary_tree_height(tree->left);
+		right_height = binary_tree_height(tree->right);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
-
-    return (left_height - right_height);
+		return (left_height - right_height);
 }
