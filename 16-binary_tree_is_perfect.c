f9394232f549069a2c746f9c930b03c8f5a7e878
ndiramiye2 <onlythenotes@gmail.com>
2024-02-29 21:59:18 +0200

    Nitafanya hivyo peke yangu

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -44,10 +44,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 		if (left_h == right_h)
 		{
-			if (binary_tree_height(tree->left) && binary_tree_height(tree->right))
+			if (tree->left == NULL && tree->right == NULL)
 				return (1);
-			else
-				return (0);
+			return (binary_tree_height(tree->left) && binary_tree_height(tree->right));
 		}
 		return (0);
 }
