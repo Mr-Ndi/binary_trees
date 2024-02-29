e81627b1d940ce9a8129fbc2653aff81eed67379
bikaze <clmntmugisha@gmail.com>
2024-02-29 23:12:56 +0200

    Updated 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -46,7 +46,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		{
 			if (tree->left == NULL && tree->right == NULL)
 				return (1);
-			return (binary_tree_height(tree->left) == binary_tree_height(tree->right));
+			return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 		}
 		return (0);
 }
